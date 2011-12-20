c56e5fef5c9a54278e9591a568628bf12c0ba8c0
Bram Bonné <bram.bonne@gmail.com>
2011-12-20 17:30:34 +0100

    Cleaned up and documented code

--- lastfmplaycount.py
+++ lastfmplaycount.py
@@ -38,61 +38,86 @@ from os import path
 LASTFM_API_KEY = "c1c872970090c90f65aed19c97519962"
 
 class LastfmPlaycountPlugin (GObject.GObject, Peas.Activatable):
-	__gtype_name__ = 'LastFmPlaycount'
-	object = GObject.property(type=GObject.GObject)
+    __gtype_name__ = 'LastFmPlaycount'
+    object = GObject.property(type=GObject.GObject)
 	
-	def __init__ (self):
-		GObject.GObject.__init__ (self)
+    def __init__ (self):
+        GObject.GObject.__init__ (self)
 
-	def do_activate (self):
-		self.shell = self.object
-		sp = self.shell.props.shell_player
-		self.emitting_uri_notify = False
-		self.db = self.shell.props.db
-		
-		self.find_username()
-		
-		self.current_entry = None
-		self.player_cb_ids = (
-			sp.connect ('playing-song-changed', self.playing_entry_changed),
-		)
-		self.playing_entry_changed (sp, sp.get_playing_entry ())
+    def do_activate (self):
+        """
+        Called when plugin is activated
+        """
+        self.emitting_uri_notify = False
+        self.db = self.object.props.db
 
-	def do_deactivate (self):
-		sp = self.shell.props.shell_player
-		for id in self.player_cb_ids:
-			sp.disconnect (id)
-		self.player_cb_ids = ()
-		
-	def find_username(self):
-	    #TODO: get this via dconf as soon as new rhythmbox APIs support it
-	    #Probably something like
-	    #client = gconf.client_get_default()
-	    #self._username = client.get_string("/apps/rhythmbox/audioscrobbler/username")
-	    config = RawConfigParser()
-	    as_session = open(path.expanduser('~/.local/share/rhythmbox/audioscrobbler/sessions'), 'r')
-	    config.readfp(as_session)
-	    self._username = config.get('Last.fm', 'username')
-	    print "Last.fm username: %s" % self._username
-		
-	def playing_entry_changed (self, sp, entry):
-	    if entry is not None:
-	        newthread = Thread(target=self.update_playcount, args=(entry,))
-	        newthread.start()
+        self._username = self.get_username()
 
-	def update_playcount (self, entry):
-		artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
-		title = entry.get_string(RB.RhythmDBPropType.TITLE)
-		#old_playcount = entry.get_int(RB.RhythmDBPropType.PLAY_COUNT)
-		playcount = self.get_playcount(artist, title)
-		print "Setting playcount for \"%s - %s\" to %d" % (artist, title, playcount)
-		self.db.entry_set(entry, RB.RhythmDBPropType.PLAY_COUNT, playcount)
-		self.db.commit()
-		
-	def get_playcount(self, artist, title):
-		params = urlencode({'method':'track.getinfo', 'api_key':LASTFM_API_KEY,
-			'artist':artist, 'track':title, 'username':self._username, 'autocorrect':1})
-		response = minidom.parse(urlopen("http://ws.audioscrobbler.com/2.0/?%s" % params))
-		playcount = response.getElementsByTagName("userplaycount")[0].childNodes[0].data
-		playcount = int(playcount)
-		return playcount
+        sp = self.object.props.shell_player
+        self.player_cb_ids = (
+	        sp.connect ('playing-song-changed', self.playing_entry_changed),
+        )
+        self.playing_entry_changed (sp, sp.get_playing_entry ())
+
+    def do_deactivate (self):
+        """
+        Called when plugin is deactivated (or when rhythmbox exits)
+        """
+        sp = self.object.props.shell_player
+        for id in self.player_cb_ids:
+            sp.disconnect (id)
+        self.player_cb_ids = ()
+	
+    def get_username(self):
+        """
+        Get the username from the session/cache of rhythmbox' audioscrobbler
+        plugin.
+        TODO: get this via dconf as soon as new rhythmbox APIs support it,
+        will probably something like
+            client = gconf.client_get_default()
+            self._username = client.get_string("/apps/rhythmbox/audioscrobbler/username")
+        """
+        config = RawConfigParser()
+        # Expanduser expands '~' into '/home/<username/'
+        as_session = open(path.expanduser('~/.local/share/rhythmbox/audioscrobbler/sessions'), 'r')
+        config.readfp(as_session)
+        username = config.get('Last.fm', 'username')
+        print "Parsed Last.fm username: %s" % username
+        return username
+	
+    def playing_entry_changed (self, sp, entry):
+        """
+        Callback function. Called whenever another song starts playing
+        @sp     Rhythmbox' shell
+        @entry  The currently playing song
+        """
+        if entry is not None:
+            # Start a new thread so UI is not blocked
+            newthread = Thread(target=self.update_playcount, args=(entry,))
+            newthread.start()
+
+    def update_playcount (self, entry):
+        """
+        Updates The database entry for the song provided
+        @entry  The song that needs to be updated
+        """
+        artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
+        title = entry.get_string(RB.RhythmDBPropType.TITLE)
+        #old_playcount = entry.get_int(RB.RhythmDBPropType.PLAY_COUNT)
+        playcount = self.get_playcount(artist, title)
+        print "Setting playcount for \"%s - %s\" to %d" % (artist, title, playcount)
+        self.db.entry_set(entry, RB.RhythmDBPropType.PLAY_COUNT, playcount)
+        self.db.commit()
+	
+    def get_playcount(self, artist, title):
+        """
+        Invokes Last.fm's API to get the playcount for the provided song
+        @artist The artist of the song
+        @title  The title of the song
+        """
+        params = urlencode({'method':'track.getinfo', 'api_key':LASTFM_API_KEY,
+            'artist':artist, 'track':title, 'username':self._username, 'autocorrect':1})
+        response = minidom.parse(urlopen("http://ws.audioscrobbler.com/2.0/?%s" % params))
+        playcount = response.getElementsByTagName("userplaycount")[0].childNodes[0].data
+        playcount = int(playcount)
+        return playcount
