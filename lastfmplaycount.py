9eeab778ab5401f48dded0386eb794f9ec554c35
Bram Bonné <bram.bonne@gmail.com>
2015-09-12 17:32:24 +0200

    Updated plugin to work with new version of Rhythmbox.

--- lastfmplaycount.py
+++ lastfmplaycount.py
@@ -26,10 +26,9 @@ import rb
 
 import gi
 from gi.repository import GObject, Gtk, Gdk, GdkPixbuf, Gio, Peas, RB
-from noconflict import classmaker
 
 from xml.dom import minidom
-from urllib import urlopen, urlencode
+import urllib
 
 from threading import Thread
 from time import sleep
@@ -39,126 +38,124 @@ from lastfmplaycountconfig import Config
 LASTFM_API_KEY = "c1c872970090c90f65aed19c97519962"
 
 class LastfmPlaycountPlugin (GObject.GObject, Peas.Activatable):
-    __gtype_name__ = 'LastFmPlaycount'
-    __metaclass__ = classmaker()
-    object = GObject.property(type=GObject.GObject)
-	
-    def __init__ (self):
-        GObject.GObject.__init__ (self)
+	object = GObject.property(type=GObject.GObject)
 
-    def do_activate (self):
-        """
-        Called when plugin is activated
-        """
-        self._updating_all = False
-        self.emitting_uri_notify = False
-        self.db = self.object.props.db
+	def __init__ (self):
+		GObject.GObject.__init__ (self)
 
-        self._config = Config()
+	def do_activate (self):
+		"""
+		Called when plugin is activated
+		"""
+		self._updating_all = False
+		self.emitting_uri_notify = False
+		self.db = self.object.props.db
 
-        sp = self.object.props.shell_player
-        self.player_cb_ids = (
-	        sp.connect ('playing-song-changed', self.playing_entry_changed),
-        )
-        self.playing_entry_changed (sp, sp.get_playing_entry ())
-        print "Activation finished"
+		self._config = Config()
 
-    def do_deactivate (self):
-        """
-        Called when plugin is deactivated (or when rhythmbox exits)
-        """
-        sp = self.object.props.shell_player
-        for id in self.player_cb_ids:
-            sp.disconnect (id)
-        self.player_cb_ids = ()
-        
-        self._config.write()
-        
-    def update_all(self):
-        """
-        Update the entire library in a separate thread.
-        Calling this function might take a while, as the last.fm service restricts
-        the maximum number of API calls per minute
-        """
-        newthread = Thread(target=self._update_all_unthreaded, args=())
-        newthread.start()
-    
-    def _update_all_unthreaded (self):
-        """
-        Update the entire library.
-        Calling this function might take a while, as the last.fm service restricts
-        the maximum number of API calls per minute
-        This is a helper function to update_all
-        """
-        if not self._updating_all:
-            print "Starting update of entire collection"
-            self._updating_all = True
-            for id in range(self.db.entry_count()):
-                self.update_entry(self.db.entry_lookup_by_id(id))
-                sleep(1)
-            self._updating_all = False
-            self.set_run_update_all(False)
-	
-    def playing_entry_changed (self, sp, entry):
-        """
-        Callback function. Called whenever another song starts playing
-        @sp     Rhythmbox' shell
-        @entry  The currently playing song
-        """
-        if entry is not None:
-            # Start a new thread so UI is not blocked
-            newthread = Thread(target=self.update_entry, args=(entry,))
-            newthread.start()
-        #Ugly hack because I can't seem to be able to access the main class in the config class
-        if self._config.get_run_update_all() and not self._updating_all:
-            print "Calling update_all"
-            self.update_all()
+		sp = self.object.props.shell_player
+		self.player_cb_ids = (
+			sp.connect ('playing-song-changed', self.playing_entry_changed),
+		)
+		self.playing_entry_changed (sp, sp.get_playing_entry ())
+		print("Activation finished")
 
-    def update_entry (self, entry):
-        """
-        Updates The database entry for the song provided
-        @entry  The song that needs to be updated
-        """
-        if entry is None:
-            return
-        artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
-        title = entry.get_string(RB.RhythmDBPropType.TITLE)
-        try:
-            playcount, lovedtrack = self.get_lastfm_info(artist, title)
-            if self._config.get_update_playcounts():
-                old_playcount = entry.get_ulong(RB.RhythmDBPropType.PLAY_COUNT)
-                if old_playcount < playcount:
-                    print "Setting playcount of \"%r - %r\" to %d" % (artist, title, playcount)
-                    self.db.entry_set(entry, RB.RhythmDBPropType.PLAY_COUNT, playcount)
-                elif old_playcount > playcount:
-                    print "Old playcount for \"%r - %r\" was higher than the new one (%d instead of %d). Not updating (assuming last.fm knows less)" % (artist, title, old_playcount, playcount)
-                else:
-                    print "Playcount for \%r - \%r remained the same. Not updating" % (artist, title)
-            if self._config.get_update_ratings() and lovedtrack:
-                print "Setting rating of \"%r - %r\" to 5 (loved track)" % (artist, title)
-                self.db.entry_set(entry, RB.RhythmDBPropType.RATING, 5)
-            self.db.commit()
-        except IOError as (errno, strerror):
-            print "Could not update \"%r - %r\ (error (%r): %s)" % (artist, title, errno, strerror)
-        
-    def get_lastfm_info(self, artist, title):
-        """
-        Invokes Last.fm's API to get the playcount for the provided song
-        @artist The artist of the song
-        @title  The title of the song
-        @return The playcount, and whether or not the track is loved
-        """
-        params = urlencode({'method':'track.getinfo', 'api_key':LASTFM_API_KEY,
-            'artist':artist, 'track':title, 'username':self._config.get_username(), 'autocorrect':1})
-        response = minidom.parse(urlopen("http://ws.audioscrobbler.com/2.0/?%s" % params))
-        try:
-            playcount = response.getElementsByTagName("userplaycount")[0].childNodes[0].data
-            playcount = int(playcount)
-        except:
-            playcount = 0
-        try:
-            lovedtrack = response.getElementsByTagName("userloved")[0].childNodes[0].data
-            lovedtrack = bool(int(lovedtrack))
-        except:
-            lovedtrack = False
-        return (playcount,lovedtrack)
+	def do_deactivate (self):
+		"""
+		Called when plugin is deactivated (or when rhythmbox exits)
+		"""
+		sp = self.object.props.shell_player
+		for id in self.player_cb_ids:
+			sp.disconnect (id)
+		self.player_cb_ids = ()
+
+		self._config.write()
+
+	def update_all(self):
+		"""
+		Update the entire library in a separate thread.
+		Calling this function might take a while, as the last.fm service restricts
+		the maximum number of API calls per minute
+		"""
+		newthread = Thread(target=self._update_all_unthreaded, args=())
+		newthread.start()
+
+	def _update_all_unthreaded (self):
+		"""
+		Update the entire library.
+		Calling this function might take a while, as the last.fm service restricts
+		the maximum number of API calls per minute
+		This is a helper function to update_all
+		"""
+		if not self._updating_all:
+			print("Starting update of entire collection")
+			self._updating_all = True
+			for id in range(self.db.entry_count()):
+				self.update_entry(self.db.entry_lookup_by_id(id))
+				sleep(1)
+			self._updating_all = False
+			self.set_run_update_all(False)
+
+	def playing_entry_changed (self, sp, entry):
+		"""
+		Callback function. Called whenever another song starts playing
+		@sp     Rhythmbox' shell
+		@entry  The currently playing song
+		"""
+		if entry is not None:
+			# Start a new thread so UI is not blocked
+			newthread = Thread(target=self.update_entry, args=(entry,))
+			newthread.start()
+		#Ugly hack because I can't seem to be able to access the main class in the config class
+		if self._config.get_run_update_all() and not self._updating_all:
+			print("Calling update_all")
+			self.update_all()
+
+	def update_entry (self, entry):
+		"""
+		Updates The database entry for the song provided
+		@entry  The song that needs to be updated
+		"""
+		if entry is None:
+			return
+		artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
+		title = entry.get_string(RB.RhythmDBPropType.TITLE)
+		try:
+			playcount, lovedtrack = self.get_lastfm_info(artist, title)
+			if self._config.get_update_playcounts():
+				old_playcount = entry.get_ulong(RB.RhythmDBPropType.PLAY_COUNT)
+				print("Setting playcount of \"%r - %r\" to %d" % (artist, title, playcount))
+				if old_playcount < playcount:
+					self.db.entry_set(entry, RB.RhythmDBPropType.PLAY_COUNT, playcount)
+				elif old_playcount > playcount:
+					print("Old playcount for \"%r - %r\" was higher than the new one (%d instead of %d). Not updating (assuming last.fm knows less)" % (artist, title, old_playcount, playcount))
+				else:
+					print("Playcount for \%r - \%r remained the same. Not updating" % (artist, title))
+			if self._config.get_update_ratings() and lovedtrack:
+				print("Setting rating of \"%r - %r\" to 5 (loved track)" % (artist, title))
+				self.db.entry_set(entry, RB.RhythmDBPropType.RATING, 5)
+			self.db.commit()
+		except IOError as e:
+			print("Could not update \"%r - %r\ (error (%r): %s)" % (artist, title, e.errno, e.strerror))
+
+	def get_lastfm_info(self, artist, title):
+		"""
+		Invokes Last.fm's API to get the playcount for the provided song
+		@artist The artist of the song
+		@title  The title of the song
+		@return The playcount, and whether or not the track is loved
+		"""
+		params = urllib.parse.urlencode({'method':'track.getinfo', 'api_key':LASTFM_API_KEY,
+			'artist':artist, 'track':title, 'username':self._config.get_username(), 'autocorrect':1})
+		response = minidom.parse(urllib.request.urlopen("http://ws.audioscrobbler.com/2.0/?%s" % params))
+		try:
+			playcount = response.getElementsByTagName("userplaycount")[0].childNodes[0].data
+			playcount = int(playcount)
+		except:
+			playcount = 0
+		try:
+			lovedtrack = response.getElementsByTagName("userloved")[0].childNodes[0].data
+			lovedtrack = bool(int(lovedtrack))
+		except:
+			lovedtrack = False
+		return (playcount,lovedtrack)
--- lastfmplaycountconfig.py
+++ lastfmplaycountconfig.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     import gconf
 
-from ConfigParser import RawConfigParser, NoSectionError
+from configparser import RawConfigParser, NoSectionError
 from os import path
 
 import rb
@@ -17,10 +17,10 @@ class Config(GObject.GObject, PeasGtk.Configurable):
     Read and write configuration data for Last.fm playcount sync plugin
     """
     __gtype_name__ = 'LastfmplaycountConfig'
-    
+
     def __init__(self):
         self._run_update_all = False #Ugly hack because I can't seem to be able to access the main class here
-    
+
         self._parse_username()
         try:
             self._gconf_client = GConf.Client.get_default()
@@ -28,12 +28,12 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         except Exception as e:
             self._gconf_client = gconf.client_get_default()
             self._gconf_client.add_dir(GCONF_DIR, gconf.CLIENT_PRELOAD_RECURSIVE)
-        
+
         self._init_config()
-        
+
     def __del__(self):
         self.write()
-        
+
     def _init_config(self):
         """
         Creates default values if none exist (this should actually be solved
@@ -45,12 +45,12 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         if self._gconf_client.get_without_default(GCONF_DIR + '/update_ratings') is None:
             self.set_update_ratings(True)
         self.set_run_update_all(False)
-            
+
     def do_create_configure_widget(self):
         """
         Called when the configuration UI button is pressed
         """
-        print "Creating configuration dialog"
+        print("Creating configuration dialog")
         builder = Gtk.Builder()
         builder.add_from_file(rb.find_plugin_file(self, "lastfmplaycount-prefs.ui"))
 
@@ -62,7 +62,7 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         builder.get_object('loved_rating').set_range(0,5)
         builder.get_object('loved_rating').set_value(5)
         builder.get_object('rating_box').set_sensitive(False)
-        
+
         callbacks = {
             "update_playcounts_toggled" : self._update_playcounts_toggled,
             "update_ratings_toggled" : self._update_ratings_toggled,
@@ -71,7 +71,7 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         builder.connect_signals(callbacks)
 
         return dialog
-        
+
     def get_username(self):
         """
         @return the user's Last.fm username
@@ -79,58 +79,58 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         if not hasattr(self, '_username') or self._username is None:
             # If the username was not filled in before, check if it is now
             self._username = self._parse_username()
-            
+
         return self._username
-        
+
     def get_update_playcounts(self):
         """
         @return Whether the user has specified that he wants his playcounts updated
         """
         return self._gconf_client.get_bool(GCONF_DIR + '/update_playcounts')
-        
+
     def set_update_playcounts(self, update):
         """
         Sets whether the user wants his playcounts to be updated
         @param update True if the user wants his playcounts to be updated
         """
-        print "Setting updating of playcounts to %r" % update
+        print("Setting updating of playcounts to %r" % update)
         self._gconf_client.set_bool(GCONF_DIR + '/update_playcounts', update)
-        
+
     def get_update_ratings(self):
         """
         @return Whether the user has specified that he wants his ratings updated
         """
         return self._gconf_client.get_bool(GCONF_DIR + '/update_ratings')
-        
+
     def set_update_ratings(self, update):
         """
         Sets whether the user wants his ratings to be updated
         @param update True if the user wants his ratings to be updated
         """
-        print "Setting updating of ratings to %r" % update
+        print( "Setting updating of ratings to %r" % update)
         self._gconf_client.set_bool(GCONF_DIR + '/update_ratings', update)
-        
+
     def get_run_update_all(self):
         """
         @return Whether the collection is being updated right now
         """
         return self._gconf_client.get_bool(GCONF_DIR + '/run_update_all')
-        
+
     def set_run_update_all(self, update):
         """
         Sets whether the collection should be updated right now
         @param update True if we should start updating the collection
         """
-        print "Run_update %r" % update
+        print("Run_update %r" % update)
         self._gconf_client.set_bool(GCONF_DIR + '/run_update_all', update)
-        
+
     def write(self):
         """
         Writes config file to permanent storage
         """
         # Not needed for gconf
         pass
-                
+
     def _parse_username(self):
         """
         Get the username from the session file of rhythmbox' audioscrobbler
@@ -142,30 +142,30 @@ class Config(GObject.GObject, PeasGtk.Configurable):
         username_config_parser.readfp(as_session)
         try:
             username = username_config_parser.get('Last.fm', 'username')
-            print "Parsed Last.fm username: %s" % username
+            print("Parsed Last.fm username: %s" % username)
             self._username = username
         except:
-            print "Error: last.fm sessions file could not be parsed. Username set to 'None'"
+            print("Error: last.fm sessions file could not be parsed. Username set to 'None'")
             self._username = None
-            
+
     def _update_playcounts_toggled(self, widget):
         """
         Callback function
         @param widget The widget containing the toggle button
         """
         enabled = widget.get_active()
-        print enabled
+        print(enabled)
         self.set_update_playcounts(enabled)
-        
+
     def _update_ratings_toggled(self, widget):
         """
         Callback function
         @param widget The widget containing the toggle button
         """
         enabled = widget.get_active()
-        print enabled
+        print(enabled)
         self.set_update_ratings(enabled)
-        
+
     def _sync_collection(self, widget):
         """
         Callback function
